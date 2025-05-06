109d0eb06d41c1e0f210a953481676a74dc468e7
progressorish <progressorish@heales.com>
2025-05-06 16:42:21 +0100

    Implemented PONG response for PING command

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
