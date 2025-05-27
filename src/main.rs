04757b51f4f01c8dbc20ab33c0017bf93e314453
progressorish <progressorish@heales.com>
2025-05-27 17:16:45 +0100

    Implemented PONG response for PING command

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,31 @@
 #![allow(unused_imports)]
-use std::io::Write; 
-use std::net::TcpListener;
+use std::io::{Read, Write};  // Line 2
+use std::net::TcpListener;   // Line 3
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
+fn main() {                  // Line 5
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
-    for stream in listener.incoming() {
-        match stream {
-            // Ok(_stream) => {
-            //     println!("accepted new connection");
-            // }
-
-            Ok(mut stream) => {
-
-                let mut buffer = [0; 1024];
-
-                loop {
-                    match stream.read(&mut buffer) {
-                       Ok(0) => break,
-                       Ok(_) => {
-                        stream.write_all(b"+PONG\r\n").unwrap();
-                        
-                       }
-                          Err(e) => {
+    for stream in listener.incoming() {     // Line 8
+        match stream {                      // Line 9
+            Ok(mut stream) => {             // Line 10
+                let mut buffer = [0; 1024]; // Line 11
+                
+                loop {                      // Line 13
+                    match stream.read(&mut buffer) {  // Line 14
+                        Ok(0) => break,
+                        Ok(_) => {
+                            stream.write_all(b"+PONG\r\n").unwrap();
+                        }
+                        Err(e) => {
                             println!("error reading from stream: {}", e);
                             break;
-                          }
-
-
-                    }
-                  
-            }
+                        }
+                    }  // Line 22: Close inner match
+                }  // Line 23: Close loop
+            }  // Line 24: Close Ok arm
             Err(e) => {
                 println!("error: {}", e);
             }
-        }
-    }
-}
+        }  // Line 28: Close outer match
+    }  // Line 29: Close for loop
+}  // Line 30: Close main
